667ca4e6148576b215339a404d0a7819b25492a4
Ales Pour <ales.pour@bonitoo.io>
2020-06-08 21:10:26 +0200

    feat: add authentication support to connection string

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package bigquery
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 )
 
@@ -11,15 +12,23 @@ func ConfigFromConnString(in string) (*Config, error) {
 	// anything else will fail
 	cfg := &Config{}
 	if strings.HasPrefix(in, "bigquery://") {
-		in = strings.ToLower(in)
-		path := strings.TrimPrefix(in, "bigquery://")
-		fields := strings.Split(path, "/")
-		if len(fields) != 3 {
+		u, err := url.Parse(in)
+		if err != nil {
+			return nil, fmt.Errorf("invalid connection string : %s (%s)", in, err.Error())
+		}
+		v, err := url.ParseQuery(u.RawQuery)
+		if err != nil {
+			return nil, fmt.Errorf("invalid connection string : %s (%s)", in, err.Error())
+		}
+		fields := strings.Split(strings.TrimPrefix(u.Path, "/"), "/")
+		if len(fields) != 2 {
 			return nil, fmt.Errorf("invalid connection string : %s", in)
 		}
-		cfg.ProjectID = fields[0]
-		cfg.Location = fields[1]
-		cfg.DataSet = fields[2]
+		cfg.ProjectID = u.Host
+		cfg.Location = fields[0]
+		cfg.DatasetID = fields[1]
+		cfg.ApiKey = v.Get("apiKey")
+		cfg.Credentials = v.Get("credentials")
 		return cfg, nil
 	} else {
 		// Nope, bad prefix
