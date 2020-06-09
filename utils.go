b1a96ae6474268b7d2e44bd05a9f42f94312fd86
Ales Pour <ales.pour@bonitoo.io>
2020-06-09 17:17:09 +0200

    style: string formatting

--- utils.go
+++ utils.go
@@ -8,21 +8,19 @@ import (
 
 // ConfigFromConnString will return the Config structures
 func ConfigFromConnString(in string) (*Config, error) {
-	// Expects format to be bigquery://projectid/location/dataset   that's IT!
-	// anything else will fail
 	cfg := &Config{}
 	if strings.HasPrefix(in, "bigquery://") {
 		u, err := url.Parse(in)
 		if err != nil {
-			return nil, fmt.Errorf("invalid connection string : %s (%s)", in, err.Error())
+			return nil, fmt.Errorf("invalid connection string: %s (%s)", in, err.Error())
 		}
 		v, err := url.ParseQuery(u.RawQuery)
 		if err != nil {
-			return nil, fmt.Errorf("invalid connection string : %s (%s)", in, err.Error())
+			return nil, fmt.Errorf("invalid connection string: %s (%s)", in, err.Error())
 		}
 		fields := strings.Split(strings.TrimPrefix(u.Path, "/"), "/")
 		if len(fields) != 2 {
-			return nil, fmt.Errorf("invalid connection string : %s", in)
+			return nil, fmt.Errorf("invalid connection string: %s", in)
 		}
 		cfg.ProjectID = u.Host
 		cfg.Location = fields[0]
